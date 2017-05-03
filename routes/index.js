84342cc327fee9e6e26c7965fdd1c3349aedc68f
MokhammadDaniyal <mokhammad.daniyal@gmail.com>
2017-05-03 00:22:18 -0700

    First Commit

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+var mongo = require('mongodb')
+var mongoose = require('mongoose');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+//MongoDB Code
+
+var Schema = new mongoose.Schema({
+    sku: String,
+    fName: String,
+    lName: String,
+    product: String,
+    phone: Number,
+    email: String,
+    postDate: Date,
+    expirationDate: Date
+})
+
+var request = mongoose.model('products',Schema);
+
+
+module.exports = router;
